80429c0010d895eaa62327fd213ec37408d312f3
Truong Nguyen <nntruong87@gmail.com>
2016-06-24 10:31:24 +0700

    Phan 2, Bai 5

---
+++ Phần 2/Bai5/Bai5/RootViewController.swift
@@ -0,0 +1,37 @@
+//
+//  RootViewController.swift
+//  Bai5
+//
+//  Created by Truong Nguyen on 6/24/16.
+//  Copyright © 2016 TruongNguyen. All rights reserved.
+//
+
+import UIKit
+
+class RootViewController: UIViewController {
+    
+    @IBOutlet weak var myView: UIView!
+    var lastLocation = CGPoint()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        if let touch = touches.first {
+            self.lastLocation = touch.locationInView(self.view)
+        }
+    }
+    
+    override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        if let touch = touches.first {
+            let location = touch.locationInView(self.view)
+            self.myView.center = CGPoint(x: (location.x - self.lastLocation.x) + self.myView.center.x, y: (location.y - self.lastLocation.y) + self.myView.center.y)
+            lastLocation = touch.locationInView(self.view)
+        }
+    }
+}
