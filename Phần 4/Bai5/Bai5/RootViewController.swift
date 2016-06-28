d4d1472a0f7702907e5fc509d7df9f2b35859711
Truong Nguyen <nntruong87@gmail.com>
2016-06-28 18:40:56 +0700

    Part 4

---
+++ Phần 4/Bai5/Bai5/RootViewController.swift
@@ -0,0 +1,27 @@
+//
+//  RootViewController.swift
+//  Bai5
+//
+//  Created by Truong Nguyen on 6/28/16.
+//  Copyright © 2016 TruongNguyen. All rights reserved.
+//
+
+import UIKit
+
+class RootViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+
+
+}
